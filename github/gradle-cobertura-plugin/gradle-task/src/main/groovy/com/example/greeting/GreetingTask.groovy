7670671328d1e00a49ded00427e365db63d58b5d
Soichiro Kashima <soichiro.kashima@gmail.com>
2015-10-26 23:04:24 +0900

    Clarify that the class is created on test.

--- github/gradle-cobertura-plugin/gradle-task/src/main/groovy/com/example/greeting/GreetingTask.groovy
+++ github/gradle-cobertura-plugin/gradle-task/src/main/groovy/com/example/greeting/GreetingTask.groovy
@@ -4,6 +4,12 @@ import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
 
 class GreetingTask extends DefaultTask {
+    GreetingTask() {
+        // This line is executed by calling 'project.evaluate()',
+        // but the coverage is zero.
+        println "Evaluating GreetingTask"
+    }
+
     @TaskAction
     void exec() {
         println "Hello, world!"
