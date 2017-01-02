0ec63088932cfa490f924519c2ce46f9c3eb0c13
Soichiro Kashima <soichiro.kashima@gmail.com>
2017-01-02 14:58:44 +0900

    Rename class in script

--- elasticsearch/bouncing-results/search.groovy
+++ elasticsearch/bouncing-results/search.groovy
@@ -14,7 +14,7 @@ class EsExportConfigProperties {
 }
 
 @Component
-class EsExport implements CommandLineRunner {
+class Search implements CommandLineRunner {
     @Autowired
     EsExportConfigProperties esConfig
 
